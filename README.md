d26117fc187fcaffcb76b5b1262ca5a410e87e28
shiyiya <shiyiya.11@gmail.com>
2023-04-05 14:04:09 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+## vercel-cors
+
+```shell
+npx vercel dev
+```
---
+++ api/cors.mjs
@@ -0,0 +1,35 @@
+import fetch from 'node-fetch'
+
+const handler = (req, res) => {
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  fetch(decodeURI(req.query.url), {
+    method: req.method,
+    body: req.body,
+    headers: {}
+  })
+    .then(async (result) => {
+      let resp, err
+
+      try {
+        resp = await result.text()
+      } catch (error) {
+        err = error
+      }
+
+      try {
+        resp = await result.json()
+      } catch (error) {
+        err = error
+      }
+
+      return resp || err
+    })
+    .then(res.send)
+    .catch((e) => res.send(e))
+}
+
+export default handler
