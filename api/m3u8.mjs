c32a3fd839e5f56e1547db5fa841d0a51bf290e3
shiyiya <shiyiya.11@gmail.com>
2023-05-03 19:14:06 +0800

    cors anywhere

--- api/m3u8.mjs
+++ api/m3u8.mjs
@@ -6,15 +6,15 @@ const handler = (req, res) => {
 
   return
 
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern; but there might not be origin (for instance call from browser)
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+  // res.setHeader('Access-Control-Allow-Credentials', true)
+  // res.setHeader('Access-Control-Allow-Origin', '*')
+  // // another common pattern; but there might not be origin (for instance call from browser)
+  // // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  // res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  // res.setHeader(
+  //   'Access-Control-Allow-Headers',
+  //   'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  // )
 
   const url = decodeURI(req.query.url)
 
