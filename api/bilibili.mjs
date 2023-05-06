b95c0a5390bbd2a71a006649aa7973b9b66c9c95
shiyiya <shiyiya.11@gmail.com>
2023-05-06 16:13:50 +0800

    typo

--- api/bilibili.mjs
+++ api/bilibili.mjs
@@ -1,15 +1,14 @@
+//@ts-nocheck
 import express from 'express'
-import request from 'request'
-
-// https://vercel.com/guides/using-express-with-vercel
+import fetch from 'node-fetch'
 
 const app = express()
 
 app.get('/api/bilibili/:vid', async (req, res) => {
-  const { data, code, message } = await request('http://api.bilibili.com/x/web-interface/view?bvid=' + req.params.vid)
+  const { message, code, data } = await (await fetch('http://api.bilibili.com/x/web-interface/view?bvid=' + req.params.vid)).json()
   if (code != 0) return res.send({ message, code, data })
 
-  res.send(await request(`https://api.bilibili.com/x/player/playurl?avid=${data.aid}&cid=${data.cid}&qn=1&type=&otype=json&platform=html5&high_quality=1`))
+  res.send(await (await fetch(`https://api.bilibili.com/x/player/playurl?avid=${data.aid}&cid=${data.cid}&qn=1&type=&otype=json&platform=html5&high_quality=1`)).json())
 
   // axios("http://api.bilibili.com/archive_stat/stat?aid="+resp.data.aid+"&type=jsonp")
 })
